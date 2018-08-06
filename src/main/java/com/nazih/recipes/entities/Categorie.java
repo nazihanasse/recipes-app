9057837f3b5c37ff2f8c9c53349d6d75a61b396f
Anasse Nazih <nazih.anasse@gmail.com>
2018-08-06 23:51:59 +0200

    Entities
    - Categorie entity

---
+++ src/main/java/com/nazih/recipes/entities/Categorie.java
@@ -0,0 +1,46 @@
+package com.nazih.recipes.entities;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Categorie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
+
+    public Categorie() {
+    }
+
+    public Categorie(String description, Set<Recipe> recipes) {
+        this.description = description;
+        this.recipes = recipes;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+}
